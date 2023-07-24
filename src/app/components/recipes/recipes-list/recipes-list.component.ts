48100c6340854d3ce291d2f9b32f103ccdcdc55c
Newton Duarte <newton_@hotmail.com>
2023-07-24 06:11:23 -0300

    feat: add recipes list array

--- src/app/components/recipes/recipes-list/recipes-list.component.ts
+++ src/app/components/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'Classic hamburger and french fries on wooden board', 'https://images.pexels.com/photos/1556688/pexels-photo-1556688.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+    new Recipe('Chicken and Vegetables', 'Dish With Rice, Chicken and Vegetables', 'https://images.pexels.com/photos/9673721/pexels-photo-9673721.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+  ];
 }
